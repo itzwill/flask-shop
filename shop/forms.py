5d73c60667a4a1aa5c0dc42119147254994e7b62
William Trimby <trimbyw@cardiff.ac.uk>
2019-03-10 20:35:42 +0000

    fixed book page

--- shop/forms.py
+++ shop/forms.py
@@ -1,13 +1,22 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField
-from wtforms.validators import DataRequired, Length, EqualTo, Email
+from wtforms.validators import Regexp, DataRequired, Length, EqualTo, Email
 
 class RegistrationForm(FlaskForm):
 	username = StringField('Username', validators=[DataRequired(), Length(min=3, max=15)])
 	email = StringField('Email', validators=[DataRequired(), Email()])
-	password = PasswordField('Password', validators=[DataRequired()])
+	password = PasswordField('Password', validators=[DataRequired(),Regexp('^.{6,10}$', message='Your password should be between 6 and 10 characters long.')])
 	confirm_password = PasswordField('Confirm Password', validators=[DataRequired(), EqualTo('password')])
 	submit = SubmitField('Register')
+	def validate_username(self, username):
+		user = User.query.filter_by(username=username.data).first()
+		if user:
+			raise ValidationError('Username already exist. Please choose a different one.')
+			
+	def validate_email(self, email):
+		email = User.query.filter_by(email.email.data).first()
+		if email:
+			raise ValidationError('This email is already associated with an account. Please use a different one.')
 	
 class LoginForm(FlaskForm):
 	email = StringField('Email', validators=[DataRequired(), Email()])
--- shop/routes.py
+++ shop/routes.py
@@ -1,10 +1,10 @@
 import os
-from flask import render_template, url_for, request, redirect
+from flask import render_template, url_for, request, redirect, session, flash
 from shop import app, db
 from shop.models import Author, Book, User
 from shop.forms import RegistrationForm, LoginForm
 from shop.models import Author, Book
-from flask_login import login_user, logout_user
+from flask_login import login_user, logout_user, LoginManager
 
 @app.route("/")
 @app.route("/home")
@@ -24,7 +24,7 @@ def book(book_id):
 @app.route("/register", methods=['GET', 'POST'])
 def register():
 	form = RegistrationForm()
-	if request.method == 'POST':
+	if form.validate_on_submit():
 		user = User(username=form.username.data, email=form.email.data, password=form.password.data)
 		db.session.add(user)
 		db.session.commit()
@@ -34,19 +34,31 @@ def register():
 @app.route("/login", methods=['GET', 'POST'])
 def login():
 	form = LoginForm()
-	if request.method == 'POST':
+	if form.validate_on_submit():
 		user = User.query.filter_by(email=form.email.data).first()
 		if user is not None and user.verify_password(form.password.data):
 			login_user(user)
+			session['logged_in'] = True
+			session['username'] = user.username
 			return redirect(url_for('home'))
 	return render_template('login.html', title='Login', form=form)
 	
 @app.route("/logout")
 def logout():
 	logout_user()
+	session.pop('username', None)
+	session['logged_in'] = False
 	return redirect(url_for('home'))
-	
+
 @app.route("/registrationthanks")
 def registrationthanks():
 	return render_template('registrationthanks.html', title='Thanks')
+	
+@app.route("/add_to_cart/<int:book_id>")
+def add_to_cart(book_id):
+	if "cart" not in session:
+		session["cart"] = []
+	session["cart"].append(book_id)
+	flash("The book is added to your shopping cart!")
+	return redirect("/cart")
 	
